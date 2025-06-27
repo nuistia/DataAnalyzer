c70f2ec54be1ae76f3261cfbc943feddc0fe26e1
Anastasiia Atanasevych <nuistia@gmail.com>
2025-06-28 01:13:22 +0300

    Add tests for string operations

---
+++ Hometask.TextAnalyzer.Core.Tests/StringOperationsTests/StringMaxNumOperationTests.cs
@@ -0,0 +1,22 @@
+﻿using Hometask.DataAnalyzer.Core.Models.ResultModels;
+using Hometask.DataAnalyzer.Core.Operation.StringOperations;
+
+namespace Hometask.DataAnalyzer.Core.Tests.StringOperationsTests;
+
+[TestFixture]
+public class StringMaxNumOperationTests
+{
+    private readonly StringMaxNumOperation _operation = new();
+
+    [Test]
+    [TestCase("Text1,skldk01dfngndfg9dfgjdjr4dfgm2klk1l;k2", 9)]
+    [TestCase("   Text1With2Spa   ces2   ", 2)]
+    public void Process_ValidText_ReturnsCorrectMaxNumber(string text, int expectedNum)
+    {
+        var expected = new IntNumberResult(expectedNum);
+
+        var actual = _operation.Process(text);
+
+        Assert.That(expected, Is.EqualTo(actual));
+    }
+}
---
+++ Hometask.TextAnalyzer.Core.Tests/StringOperationsTests/StringNumSumOperationsTests.cs
@@ -0,0 +1,22 @@
+﻿using Hometask.DataAnalyzer.Core.Models.ResultModels;
+using Hometask.DataAnalyzer.Core.Operation.StringOperations;
+
+namespace Hometask.DataAnalyzer.Core.Tests.StringOperationsTests;
+
+[TestFixture]
+public class StringNumSumOperationTests
+{
+    private readonly StringNumSumOperation _operation = new();
+
+    [Test]
+    [TestCase("Text1,skldk01dfngndfg9dfgjdjr4dfgm2klk1l;k2", 20)]
+    [TestCase("   Text1With2Spa   ces2   ", 5)]
+    public void Process_ValidText_ReturnsCorrectSum(string text, int expectedSum)
+    {
+        var expected = new IntNumberResult(expectedSum);
+
+        var actual = _operation.Process(text);
+
+        Assert.That(expected, Is.EqualTo(actual));
+    }
+}
