8d307eb85cb0e821df36d80ba8309c3b5dc1f831
Anastasiia Atanasevych <nuistia@gmail.com>
2025-06-28 18:29:04 +0300

    Add tests for existing array operations

---
+++ Hometask.DataAnalyzer.Core.Tests/ArrayOperationsTests/BooksArrayOperationsTests/ArrayThickestBookOperationTests.cs
@@ -0,0 +1,22 @@
+﻿using Hometask.DataAnalyzer.Core.Models.BookModels;
+using Hometask.DataAnalyzer.Core.Models.ResultModels;
+using Hometask.DataAnalyzer.Core.Operation.ArrayOperations.BooksArrayOperations;
+
+namespace Hometask.DataAnalyzer.Core.Tests.ArrayOperationsTests.BooksArrayOperationsTests;
+
+[TestFixture]
+public class ArrayThickestBookOperationTests
+{
+    private readonly Book[] _testBooks = BookTestData.GetTestBooks();
+    private readonly ArrayThickestBookOperation _operation = new();
+
+    [Test]
+    public void Process_ValidBooks_ReturnsFirstThickestBookIndex()
+    {
+        IntNumberResult expectedResult = new(500);
+
+        var actual = _operation.Process(_testBooks);
+
+        Assert.That(expectedResult.Value, Is.EqualTo(actual.Value));
+    }
+}
---
+++ Hometask.DataAnalyzer.Core.Tests/ArrayOperationsTests/CarArrayOperationsTests/ArrayFastestCarOperationTests.cs
@@ -0,0 +1,23 @@
+﻿using Hometask.DataAnalyzer.Core.Models.CarModels;
+using Hometask.DataAnalyzer.Core.Models.ResultModels;
+using Hometask.DataAnalyzer.Core.Operation.ArrayOperations.CarArrayOperations;
+
+namespace Hometask.DataAnalyzer.Core.Tests.ArrayOperationsTests.CarArrayOperationsTests;
+
+[TestFixture]
+public class ArrayFastestCarOperationTests
+{
+    private readonly Car[] _testCars = CarTestData.GetTestCars();
+    private readonly ArrayFastestCarOperation _operation = new();
+
+    [Test]
+    public void Process_ValidBooks_ReturnsFirstThickestBookIndex()
+    {
+        FastestCarResult expectedResult = new(4, 36);
+
+        var actual = _operation.Process(_testCars);
+
+        Assert.That(expectedResult.FirstIndex, Is.EqualTo(actual.FirstIndex));
+        Assert.That(expectedResult.LastIndex, Is.EqualTo(actual.LastIndex));
+    }
+}
